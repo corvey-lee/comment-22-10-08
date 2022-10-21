18e0805cf4d04a3bfc6248edb461b183e0139e47
hyeonhyeong.park <hyeonhyeong.park@hyeonkyeongparknoMacBook-Pro.local>
2022-10-21 10:54:05 +0900

    동기 비동기 테스트 구분

--- main_test.go
+++ main_test.go
@@ -6,14 +6,18 @@ import (
 	"time"
 )
 
-func TestTiming(t *testing.T) {
+func TestAsyncTiming(t *testing.T) {
+	fileListCount := 100
+
+	start := time.Now()
+	Async(fileListCount)
+	log.Println("비동기 걸린 시간: ", time.Now().Sub(start))
+}
+
+func TestSyncTiming(t *testing.T) {
 	fileListCount := 100
 
 	start := time.Now()
 	Sync(fileListCount)
 	log.Println("동기 걸린 시간: ", time.Now().Sub(start))
-
-	start = time.Now()
-	Async(fileListCount)
-	log.Println("비동기 걸린 시간: ", time.Now().Sub(start))
 }
