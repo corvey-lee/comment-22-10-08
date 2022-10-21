99c2efd73616a06dddea1b681b4929e1df4e3d14
hyeonhyeong.park <hyeonhyeong.park@hyeonkyeongparknoMacBook-Pro.local>
2022-10-21 10:55:58 +0900

    path cashe 대응

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -11,20 +14,38 @@ import (
 func main() {
 	fileListCount := 100
 
-	// fmt.Println(Sync(fileListCount))
-	fmt.Println("Romeo 단어의 총 갯수: ", Async(fileListCount))
+	// fmt.Println("(동기) Romeo 단어의 총 갯수: ", Sync(fileListCount))
+	fmt.Println("(비동기) Romeo 단어의 총 갯수: ", Async(fileListCount))
 }
 
 func Async(fileListCount int) int {
 	total := 0
 	ch := make(chan int, fileListCount)
 	wg := sync.WaitGroup{}
+	temp_path := "./temp"
+
+	// temp 폴더 생성
+	if _, err := os.Stat(temp_path); os.IsNotExist(err) {
+		err := os.Mkdir(temp_path, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	// temp 폴더에 sampleN.txt 파일 생성하고 삭제
 	for i := 0; i < fileListCount; i++ {
-		path := "./sample.txt"
+		path := temp_path + "/sample" + strconv.Itoa(i) + ".txt"
+		cmd := exec.Command("cp", "./sample.txt", path)
+		_, err := cmd.Output()
+		if err != nil {
+			log.Println(err)
+		}
+
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			ch <- CountingStrings(path)
+			ch <- countingStrings(path)
+			os.Remove(path)
 		}()
 	}
 	wg.Wait()
@@ -34,21 +55,43 @@ func Async(fileListCount int) int {
 		total += i
 	}
 
+	// select {
+	// case i := <-ch:
+	// 	total += i
+	// }
+
 	return total
 }
 
 func Sync(fileListCount int) int {
 	total := 0
+	temp_path := "./temp"
 
+	// temp 폴더 생성
+	if _, err := os.Stat(temp_path); os.IsNotExist(err) {
+		err := os.Mkdir(temp_path, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	// temp 폴더에 sampleN.txt 파일 생성하고 삭제
 	for i := 0; i < fileListCount; i++ {
-		path := "./sample.txt"
-		total += CountingStrings(path)
+		path := temp_path + "/sample" + strconv.Itoa(i) + ".txt"
+		cmd := exec.Command("cp", "./sample.txt", path)
+		_, err := cmd.Output()
+		if err != nil {
+			log.Println(err)
+		}
+
+		total += countingStrings(path)
+		os.Remove(path)
 	}
 
 	return total
 }
 
-func CountingStrings(path string) int {
+func countingStrings(path string) int {
 	sum := 0
 
 	f, err := os.Open(path)
